fecf5f75a8c61a805c27567624d7dcfe3f9f3cb8
estojanovic <estojanovic8720rn@raf.rs>
2023-03-08 01:42:21 +0100

    Dodavanje doktor komponente - radni prostor - radni prostor za jednog pacijenta - zdravstveni karton - pretraga pacijenata

---
+++ src/app/components/doctor/doctor-medical-chart/doctor-medical-chart.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-medical-chart',
+  templateUrl: './doctor-medical-chart.component.html',
+  styleUrls: ['./doctor-medical-chart.component.css']
+})
+export class DoctorMedicalChartComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/doctor/doctor-workspace-one-patient/doctor-workspace-one-patient.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-workspace-one-patient',
+  templateUrl: './doctor-workspace-one-patient.component.html',
+  styleUrls: ['./doctor-workspace-one-patient.component.css']
+})
+export class DoctorWorkspaceOnePatientComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
