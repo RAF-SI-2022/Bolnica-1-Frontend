bdb443df06b75f626b01a9de591bf93cb873d12d
estojanovic <estojanovic8720rn@raf.rs>
2023-03-08 01:35:59 +0100

    Dodavanje admin stranica - dodavanje radnika - editovanje radnika - pretraga radnika - radni prostor

---
+++ src/app/components/admin/admin-add-employee/admin-add-employee.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-admin-add-employee',
+  templateUrl: './admin-add-employee.component.html',
+  styleUrls: ['./admin-add-employee.component.css']
+})
+export class AdminAddEmployeeComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/admin/admin-search-employee/admin-search-employee.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-admin-search-employee',
+  templateUrl: './admin-search-employee.component.html',
+  styleUrls: ['./admin-search-employee.component.css']
+})
+export class AdminSearchEmployeeComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
