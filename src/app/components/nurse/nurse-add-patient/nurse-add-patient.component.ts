30cd06babe056d25a0d9088cb422d95125c3dea0
estojanovic <estojanovic8720rn@raf.rs>
2023-03-08 01:46:39 +0100

    Dodavanje medicinske sestre - kreiranje novog pacijenta - prijem pacijenta - zakazivanje prijema - zakazivanje pregleda - pretraga pacijenata - pretraga pacijenata po odeljenjima - radni prostor

---
+++ src/app/components/nurse/nurse-add-patient/nurse-add-patient.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-nurse-add-patient',
+  templateUrl: './nurse-add-patient.component.html',
+  styleUrls: ['./nurse-add-patient.component.css']
+})
+export class NurseAddPatientComponent implements OnInit{
+
+  ngOnInit(): void {
+  }
+
+}
+
