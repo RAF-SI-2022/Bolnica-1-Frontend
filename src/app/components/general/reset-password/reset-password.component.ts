e6398bfc6711a90f3a483cc077929f637d6a561e
estojanovic <estojanovic8720rn@raf.rs>
2023-03-08 01:30:56 +0100

    Dodavanje reset-password komponente

---
+++ src/app/components/general/reset-password/reset-password.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.css']
+})
+export class ResetPasswordComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
