2c23a45d0f1b32d9d28042fcd8e38a8ddc6f5f88
estojanovic <estojanovic8720rn@raf.rs>
2023-03-08 12:13:51 +0100

    Dodavanje ruta ka stranicama - editovanje routing.module.ts fajla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,48 @@ import {
 import {
   TechnicianWorkspaceComponent
 } from "./components/technician/technician-workspace/technician-workspace.component";
+import {AdminSearchEmployeeComponent} from "./components/admin/admin-search-employee/admin-search-employee.component";
+import {ProfileComponent} from "./components/general/profile/profile.component";
+import {AdminEditEmployeeComponent} from "./components/admin/admin-edit-employee/admin-edit-employee.component";
+import {DoctorMedicalChartComponent} from "./components/doctor/doctor-medical-chart/doctor-medical-chart.component";
+import {
+  DoctorSearchPatientsComponent
+} from "./components/doctor/doctor-search-patients/doctor-search-patients.component";
+import {
+  DoctorWorkspaceOnePatientComponent
+} from "./components/doctor/doctor-workspace-one-patient/doctor-workspace-one-patient.component";
+import {NurseAddPatientComponent} from "./components/nurse/nurse-add-patient/nurse-add-patient.component";
+import {
+  NursePatientAdmissionComponent
+} from "./components/nurse/nurse-patient-admission/nurse-patient-admission.component";
+import {
+  NurseScheduleAdmissionComponent
+} from "./components/nurse/nurse-schedule-admission/nurse-schedule-admission.component";
+import {
+  NurseScheduleAppointmentComponent
+} from "./components/nurse/nurse-schedule-appointment/nurse-schedule-appointment.component";
+import {NurseSearchPatientsComponent} from "./components/nurse/nurse-search-patients/nurse-search-patients.component";
+import {
+  NurseSearchPatientsDepartmentsComponent
+} from "./components/nurse/nurse-search-patients-departments/nurse-search-patients-departments.component";
+import {
+  ReceptionistAddPatientComponent
+} from "./components/receptionist/receptionist-add-patient/receptionist-add-patient.component";
+import {
+  ReceptionistScheduleAppointmentComponent
+} from "./components/receptionist/receptionist-schedule-appointment/receptionist-schedule-appointment.component";
+import {
+  ReceptionistSearchPatientsComponent
+} from "./components/receptionist/receptionist-search-patients/receptionist-search-patients.component";
+import {
+  TechnicianIssuingResultsComponent
+} from "./components/technician/technician-issuing-results/technician-issuing-results.component";
+import {
+  TechnicianPatientAdmissionComponent
+} from "./components/technician/technician-patient-admission/technician-patient-admission.component";
+import {
+  TechnicianScheduleVisitComponent
+} from "./components/technician/technician-schedule-visit/technician-schedule-visit.component";
 
 
 const routes: Routes = [
@@ -29,8 +71,23 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
+    path: "profile",
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {
     path: "admin-add-employee",
     component: AdminAddEmployeeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "admin-edit-employee",
+    component: AdminEditEmployeeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "admin-search-employee",
+    component: AdminSearchEmployeeComponent,
     canActivate: [AuthGuard]
   },
   {
@@ -44,8 +101,53 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
+    path: "doctor-medical-chart",
+    component: DoctorMedicalChartComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "doctor-search-patients",
+    component: DoctorSearchPatientsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
     path: "doctor-workspace",
     component: DoctorWorkspaceComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "doctor-workspace-one",
+    component: DoctorWorkspaceOnePatientComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "nurse-add-patient",
+    component: NurseAddPatientComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "nurse-patient-admission",
+    component: NursePatientAdmissionComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "nurse-schedule-admission",
+    component: NurseScheduleAdmissionComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "nurse-schedule-appointment",
+    component: NurseScheduleAppointmentComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "nurse-search-patients",
+    component: NurseSearchPatientsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "nurse-search-patients-dep",
+    component: NurseSearchPatientsDepartmentsComponent,
     canActivate: [AuthGuard]
   },
   {
@@ -54,8 +156,38 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
+    path: "receptionist-add-patient",
+    component: ReceptionistAddPatientComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "receptionist-schedule-appointment",
+    component: ReceptionistScheduleAppointmentComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "receptionist-search-patients",
+    component: ReceptionistSearchPatientsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
     path: "receptionist-workspace",
     component: ReceptionistWorkspaceComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "technician-issuing-results",
+    component: TechnicianIssuingResultsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "technician-patient-admission",
+    component: TechnicianPatientAdmissionComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "technician-schedule-visit",
+    component: TechnicianScheduleVisitComponent,
     canActivate: [AuthGuard]
   },
   {
