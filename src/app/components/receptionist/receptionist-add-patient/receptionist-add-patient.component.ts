d325900321921654d1805cbbde24bfecc9a65322
estojanovic <estojanovic8720rn@raf.rs>
2023-03-08 01:53:41 +0100

    Dodavanje recepcioner stranica - novi pacijent - zakazivanje pregleda - pretraga pacijenata - radni prostor

---
+++ src/app/components/receptionist/receptionist-add-patient/receptionist-add-patient.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-receptionist-add-patient',
+  templateUrl: './receptionist-add-patient.component.html',
+  styleUrls: ['./receptionist-add-patient.component.css']
+})
+export class ReceptionistAddPatientComponent implements OnInit{
+
+  ngOnInit(): void {
+  }
+
+}
+
---
+++ src/app/components/receptionist/receptionist-schedule-appointment/receptionist-schedule-appointment.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-receptionist-schedule-appointment',
+  templateUrl: './receptionist-schedule-appointment.component.html',
+  styleUrls: ['./receptionist-schedule-appointment.component.css']
+})
+export class ReceptionistScheduleAppointmentComponent implements OnInit{
+
+  ngOnInit(): void {
+  }
+
+}
