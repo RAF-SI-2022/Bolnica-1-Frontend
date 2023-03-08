2be3b714540a9c93f5f7c6a58522a4376a6b1cff
estojanovic <estojanovic8720rn@raf.rs>
2023-03-08 01:50:23 +0100

    Dodavanje tehnicar komponente - prijem pacijenta - zakazivanje posete - radna povrsina - izdavanje rezultata lab analiza

---
+++ src/app/components/technician/technician-patient-admission/technician-patient-admission.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-technician-patient-admission',
+  templateUrl: './technician-patient-admission.component.html',
+  styleUrls: ['./technician-patient-admission.component.css']
+})
+export class TechnicianPatientAdmissionComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/technician/technician-workspace/technician-workspace.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-technician-workspace',
+  templateUrl: './technician-workspace.component.html',
+  styleUrls: ['./technician-workspace.component.css']
+})
+export class TechnicianWorkspaceComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
