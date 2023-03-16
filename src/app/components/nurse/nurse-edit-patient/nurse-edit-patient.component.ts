29ddd04c2a2da7982c21113a6d2fb96a66e994b2
estojanovic <estojanovic8720rn@raf.rs>
2023-03-16 22:52:03 +0100

    dodata nurse-edit-componenta

---
+++ src/app/components/nurse/nurse-edit-patient/nurse-edit-patient.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-nurse-edit-patient',
+  templateUrl: './nurse-edit-patient.component.html',
+  styleUrls: ['./nurse-edit-patient.component.css']
+})
+export class NurseEditPatientComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
