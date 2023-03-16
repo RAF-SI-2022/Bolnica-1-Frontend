341eddfb5435f4b1cdf5f373442ab5254c5e99de
estojanovic <estojanovic8720rn@raf.rs>
2023-03-17 00:42:03 +0100

    dodata refferal comp

---
+++ src/app/components/doctor/doctor-create-referral/doctor-create-referral.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-create-referral',
+  templateUrl: './doctor-create-referral.component.html',
+  styleUrls: ['./doctor-create-referral.component.css']
+})
+export class DoctorCreateReferralComponent {
+
+}
