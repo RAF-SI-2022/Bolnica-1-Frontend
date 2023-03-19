8837c7fc832c0cb949d7c427a243877ecff8b5f8
andreajo00 <ajovanovic9720rn@raf.rs>
2023-03-19 22:57:39 +0100

    [View] [LS] Radni prostor za rad sa pacijentom #25

--- src/app/components/doctor/doctor-workspace-one-patient/doctor-workspace-one-patient.component.ts
+++ src/app/components/doctor/doctor-workspace-one-patient/doctor-workspace-one-patient.component.ts
@@ -6,6 +6,20 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./doctor-workspace-one-patient.component.css']
 })
 export class DoctorWorkspaceOnePatientComponent implements OnInit {
+  isPopupVisible = false;
+
+  showPopup(event: any) {
+    this.isPopupVisible = true;
+  }
+
+  hidePopup() {
+    this.isPopupVisible = false;
+  }
+
+  confirmSacuvaj() {
+    //treba sacuvati izvestaj
+
+  }
 
   ngOnInit(): void {
   }
