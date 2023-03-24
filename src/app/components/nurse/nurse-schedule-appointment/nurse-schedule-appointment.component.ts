feb46dbf9d5296f3f8362e1fe2aeb9c6440d16d1
andreajo00 <ajovanovic9720rn@raf.rs>
2023-03-24 15:46:47 +0100

    [View] [MS/VMS] Zakazivanje pregleda #66

--- src/app/components/nurse/nurse-schedule-appointment/nurse-schedule-appointment.component.ts
+++ src/app/components/nurse/nurse-schedule-appointment/nurse-schedule-appointment.component.ts
@@ -1,13 +1,42 @@
 import {Component, OnInit} from '@angular/core';
 
+//import {View} from '@syncfusion/ej2-angular-schedule';
+import {NgxPaginationModule} from "ngx-pagination";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+declare let $: any;
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 @Component({
   selector: 'app-nurse-schedule-appointment',
+  //template: '<ejs-schedule></ejs-schedule>',
   templateUrl: './nurse-schedule-appointment.component.html',
   styleUrls: ['./nurse-schedule-appointment.component.css']
 })
 export class NurseScheduleAppointmentComponent implements OnInit{
 
+  calendarOptions: CalendarOptions ={
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin],
+    //dateClick: this.handleDateClick.bind(this),
+    events: [
+      { title: 'event 1', date: '2020-11-05' },
+      { title: 'event 2', date: '2020-06-30' }
+    ]
+  };
+
+  /*
+  handleDateClick(arg) {
+    alert('date click! ' + arg.dateStr)
+  }
+*/
+
+  //public setView: View = 'Month';
+
+
+
   ngOnInit(): void {
   }
+
 
 }
