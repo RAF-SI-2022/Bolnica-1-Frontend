8d8b4073295230a3b9c75f790ab5e4084bd7819f
andreajo00 <ajovanovic9720rn@raf.rs>
2023-03-31 12:29:26 +0200

    [View] [LS] Radni prostor za kreiranje uputa #58

--- src/app/components/doctor/doctor-create-referral/doctor-create-referral.component.ts
+++ src/app/components/doctor/doctor-create-referral/doctor-create-referral.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-doctor-create-referral',
   templateUrl: './doctor-create-referral.component.html',
   styleUrls: ['./doctor-create-referral.component.css']
 })
-export class DoctorCreateReferralComponent {
+export class DoctorCreateReferralComponent implements OnInit{
+/*
+  selectedOption: string;
+
+  onOptionSelected(value: string) {
+    this.selectedOption = value;
+  }
+*/
+  isPopupVisible = false;
+
+  showPopup(event: any) {
+    this.isPopupVisible = true;
+  }
+
+  hidePopup() {
+    this.isPopupVisible = false;
+  }
+  confirmUput() {
+    //za slanje uputa
+
+  }
+  ngOnInit(): void {
+  }
+
 
 }
