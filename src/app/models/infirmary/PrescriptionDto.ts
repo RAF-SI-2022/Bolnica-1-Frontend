5af7293f2483e45e5f61c8b7ccc956d28bd44d23
estojanovic <estojanovic8720rn@raf.rs>
2023-05-03 21:59:24 +0200

    sestra prijem

--- src/app/models/infirmary/PrescriptionDto.ts
+++ src/app/models/infirmary/PrescriptionDto.ts
@@ -1,5 +1,6 @@
 import {PrescriptionType} from "../laboratory-enums/PrescriptionType";
 import {PrescriptionStatus} from "../laboratory-enums/PrescriptionStatus";
+import {PrescriptionAnalysisData} from "../laboratory/PrescriptionAnalysisData";
 
 export interface PrescriptionDto{
   id: number;
@@ -12,5 +13,19 @@ export interface PrescriptionDto{
   status: PrescriptionStatus;
   referralDiagnosis: string;
   referralReason: string;
+
+}
+
+export class PrescriptionDto{
+  id: number = 0;
+  doctorLbz: string = '';
+  departmentFromId: number = 0;
+  departmentToId: number = 0;
+  lbp: string = '';
+  creationDateTime: Date =  new Date; //ovo je Timestamp
+  type: PrescriptionType = PrescriptionType.STACIONAR;
+  status: PrescriptionStatus = PrescriptionStatus.NEREALIZOVAN;
+  referralDiagnosis: string = '';
+  referralReason: string= '';
 
 }
