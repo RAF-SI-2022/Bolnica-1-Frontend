f5fdf2508290791d1a48bce1a0a94b8dc704bd37
estojanovic <estojanovic8720rn@raf.rs>
2023-05-19 21:20:35 +0200

    cypress testovi

---
+++ cypress/e2e/doctor-infirmary.cy.ts
@@ -0,0 +1,8 @@
+// pretraziti pacijente na stacionaru
+
+// klikom na red u tabeli -> otvori se novi prozor
+  // kreirati uput za laboratoriju
+  // kreirati zdravstveni izvestaj
+  // videti istoriju stanja
+  // kreirati otpusnu listu
+  // proveriti da li je nova otpusna lista u zdravstvenom kartonu u tabu istorija otpusnih listi
