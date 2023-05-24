c06eb1875cc5fb80df6aecb7cce84056f91dface
estojanovic <estojanovic8720rn@raf.rs>
2023-05-25 00:19:56 +0200

    rute ingres

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,24 +1,24 @@
 export const environment = {
     production: false,
     title: 'Local Environment',
-    apiURL: 'http://localhost:8080/api'
+    apiURL: 'https://bolnica-1.k8s.elab.rs/api/employees'
   };
 
 export const environmentPatient = {
   production: false,
   title: 'Local Patient Environment',
-  apiURL: 'http://localhost:8081/api'
+  apiURL: 'https://bolnica-1.k8s.elab.rs/api/patients'
 };
 
 export const environmentLaboratory = {
   production: false,
   title: 'Local Laboratory Environment',
-  apiURL: 'http://localhost:8083/api/laboratory'
+  apiURL: 'https://bolnica-1.k8s.elab.rs/api/laboratory'
 };
 
 export const environmentInfirmary = {
   production: false,
   title: 'Local Infirmary Environment',
-  apiURL: 'http://localhost:8084/api/infirmary'
+  apiURL: 'https://bolnica-1.k8s.elab.rs/api/infirmary'
 };
 
